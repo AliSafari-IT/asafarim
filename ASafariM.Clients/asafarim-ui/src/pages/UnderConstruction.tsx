3c6c47c3480dd188fcd6d3ba1d9f28894ef748e6
asafarim <asafarim.it@gmail.com>
2025-02-16 23:32:50 +0100

    up

--- ASafariM.Clients/asafarim-ui/src/pages/UnderConstruction.tsx
+++ ASafariM.Clients/asafarim-ui/src/pages/UnderConstruction.tsx
@@ -1,5 +1,3 @@
- import React from 'react';
-
 const UnderConstruction = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>

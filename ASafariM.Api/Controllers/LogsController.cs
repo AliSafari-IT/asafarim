678b3a5dbb504309ceb1f15aff4754bd5507656a
Ali <asafarim@gmail.com>
2025-02-24 13:30:18 +0000

    chore(controllers): update route for LogsController

--- ASafariM.Api/Controllers/LogsController.cs
+++ ASafariM.Api/Controllers/LogsController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/logs")]
 public class LogsController : ControllerBase
 {
     private readonly ILogger<LogsController> _logger;

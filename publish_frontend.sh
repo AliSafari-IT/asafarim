b85f59366ee28efc18924cb7f89aa18898a384e1
Ali Safari <Asafarim@gmail.com>
2025-02-27 15:40:35 +0100

    chore(package): update package.json scripts

--- publish_frontend.sh
+++ publish_frontend.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+# clean and install asafarim-ui working directory
 if [ -d /var/www/asafarim.com/public_html ]; then
     sudo rm -rf /var/www/asafarim.com/public_html/*
 fi
-
-cd /var/www/asafarim/ASafariM.Clients/asafarim-ui
+# clean and install root working directory and asafarim-ui
+cd /var/www/asafarim
 yarn clean
 yarn install
-yarn build
+yarn run ui:reinstall
+yarn run ui:build
+
+# build asafarim-ui working directory
+cd /var/www/asafarim/ASafariM.Clients/asafarim-ui
 sudo cp -r dist/* /var/www/asafarim.com/public_html/
 sudo chown -R www-data:www-data /var/www/asafarim.com/public_html
 sudo chmod -R 755 /var/www/asafarim.com/public_html
 
 # restart nginx
 sudo systemctl restart nginx
-# check if nginx is running
-sudo systemctl status nginx
+

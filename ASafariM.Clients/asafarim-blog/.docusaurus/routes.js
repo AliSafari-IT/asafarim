49b1c962a0d78101c1bab2872571b872e8ade9a5
Ali Safari <Asafarim@gmail.com>
2025-03-21 17:56:26 +0100

    chore(ASafariM.Clients/asafarim-blog): update blog post default settings and add new blog post and authors and tags files

--- ASafariM.Clients/asafarim-blog/.docusaurus/routes.js
+++ ASafariM.Clients/asafarim-blog/.docusaurus/routes.js
@@ -38,33 +38,77 @@ export default [
     exact: true
   },
   {
+    path: '/asafarim-blog/blog',
+    component: ComponentCreator('/asafarim-blog/blog', 'e0e'),
+    exact: true
+  },
+  {
+    path: '/asafarim-blog/blog/archive',
+    component: ComponentCreator('/asafarim-blog/blog/archive', '784'),
+    exact: true
+  },
+  {
+    path: '/asafarim-blog/blog/authors',
+    component: ComponentCreator('/asafarim-blog/blog/authors', '20e'),
+    exact: true
+  },
+  {
+    path: '/asafarim-blog/blog/tags',
+    component: ComponentCreator('/asafarim-blog/blog/tags', '45c'),
+    exact: true
+  },
+  {
+    path: '/asafarim-blog/blog/tags/asafarim',
+    component: ComponentCreator('/asafarim-blog/blog/tags/asafarim', '952'),
+    exact: true
+  },
+  {
+    path: '/asafarim-blog/blog/tags/blog',
+    component: ComponentCreator('/asafarim-blog/blog/tags/blog', '085'),
+    exact: true
+  },
+  {
+    path: '/asafarim-blog/blog/tags/welcome',
+    component: ComponentCreator('/asafarim-blog/blog/tags/welcome', 'a40'),
+    exact: true
+  },
+  {
+    path: '/asafarim-blog/blog/welcome-to-asafarim-blog',
+    component: ComponentCreator('/asafarim-blog/blog/welcome-to-asafarim-blog', '861'),
+    exact: true
+  },
+  {
     path: '/asafarim-blog/markdown-page',
     component: ComponentCreator('/asafarim-blog/markdown-page', '2f1'),
     exact: true
   },
   {
     path: '/asafarim-blog/docs',
-    component: ComponentCreator('/asafarim-blog/docs', '4fd'),
+    component: ComponentCreator('/asafarim-blog/docs', '8ee'),
     routes: [
       {
         path: '/asafarim-blog/docs',
-        component: ComponentCreator('/asafarim-blog/docs', '026'),
+        component: ComponentCreator('/asafarim-blog/docs', 'd7b'),
         routes: [
           {
             path: '/asafarim-blog/docs',
-            component: ComponentCreator('/asafarim-blog/docs', '954'),
+            component: ComponentCreator('/asafarim-blog/docs', 'ae8'),
             routes: [
               {
                 path: '/asafarim-blog/docs/intro',
-                component: ComponentCreator('/asafarim-blog/docs/intro', '33c'),
-                exact: true,
-                sidebar: "tutorialSidebar"
+                component: ComponentCreator('/asafarim-blog/docs/intro', '331'),
+                exact: true
               }
             ]
           }
         ]
       }
     ]
+  },
+  {
+    path: '/asafarim-blog/',
+    component: ComponentCreator('/asafarim-blog/', '168'),
+    exact: true
   },
   {
     path: '*',

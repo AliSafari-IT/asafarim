83a1b6e0b873ede49944e20a8f4d2071ef58f58f
Ali Safari <Asafarim@gmail.com>
2025-03-23 00:13:02 +0100

    Remove .docusaurus directory from repository

--- ASafariM.Clients/asafarim-blog/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("D:\\repos\\ASafariM\\ASafariM.Clients\\asafarim-blog\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\repos\\ASafariM\\ASafariM.Clients\\asafarim-blog\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\repos\\ASafariM\\ASafariM.Clients\\asafarim-blog\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\repos\\ASafariM\\ASafariM.Clients\\asafarim-blog\\src\\css\\custom.css"),
-];

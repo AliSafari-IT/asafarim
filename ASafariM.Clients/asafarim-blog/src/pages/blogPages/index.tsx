46a34a880104b1998cedc191f7ebf07cb5dd80b8
Ali Safari <Asafarim@gmail.com>
2025-04-10 15:54:57 +0200

    chore(ASafariM.Clients): add new blog pages and styles module CSS

---
+++ ASafariM.Clients/asafarim-blog/src/pages/blogPages/index.tsx
@@ -0,0 +1,7 @@
+import BalancedView from "./BalancedView";
+import ReceiptImage from "./ReceiptImage";
+import ReferralCard from "./ReferralCard";
+import EnhancedReferralCard from "./EnhancedReferralCard";
+
+export { BalancedView, ReceiptImage, ReferralCard, EnhancedReferralCard };
+export default { BalancedView, ReceiptImage, ReferralCard, EnhancedReferralCard };

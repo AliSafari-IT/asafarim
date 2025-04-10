3343481c1cb23602b52dbd569aa82d1e3cc445ac
Ali Safari <Asafarim@gmail.com>
2025-04-10 17:51:25 +0200

    Add blog posts analyzing Windsurf AI coding assistant experience and feedback

--- ASafariM.Clients/asafarim-blog/src/pages/blogPages/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export { default as BalancedView } from "./BalancedView";
-export { default as ReceiptImage } from "./ReceiptImage"; 
-export { default as ReferralCard } from "./ReferralCard";
-export { default as EnhancedReferralCard } from "./EnhancedReferralCard";

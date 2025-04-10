c9e032c1165352b6a69cdd515949a812fb423537
Ali <asafarim@gmail.com>
2025-04-10 15:14:37 +0000

    Convert Windsurf blog post from .md to .mdx and update package.json script order

--- ASafariM.Clients/asafarim-blog/src/pages/blogPages/index.tsx
+++ ASafariM.Clients/asafarim-blog/src/pages/blogPages/index.tsx
@@ -1,7 +1,7 @@
+import React from 'react';
 import BalancedView from "./BalancedView";
 import ReceiptImage from "./ReceiptImage";
 import ReferralCard from "./ReferralCard";
 import EnhancedReferralCard from "./EnhancedReferralCard";
 
-export { BalancedView, ReceiptImage, ReferralCard, EnhancedReferralCard };
-export default { BalancedView, ReceiptImage, ReferralCard, EnhancedReferralCard };
+export default { BalancedView, ReceiptImage, ReferralCard, EnhancedReferralCard }
